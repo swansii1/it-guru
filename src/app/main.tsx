0f3b65ea13e94f6dd88e15f23246943a5e04f273
Matvey <matvey.swansii@gmail.com>
2026-03-23 15:55:49 +0300

    feat: add default options for query client

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,7 +6,16 @@ import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 100,
+      refetchOnReconnect: true,
+      refetchOnMount: false,
+      gcTime: 10 * 60 * 1000,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
